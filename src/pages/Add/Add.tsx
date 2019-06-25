c769f07686cdb0866ebf7e608bdf360d4ab1ba1c
succie <exactf1ash503@gmail.com>
2019-06-25 23:16:17 +0900

    Add article add page

---
+++ src/pages/Add/Add.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useCallback } from "react";
+import firebase from "firebase/app";
+import { firestore } from "../../utils/firebase";
+import { Article } from "../../store/articles";
+
+const Add = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [genre, setGenre] = useState("");
+
+  const post = useCallback(async () => {
+    const timestamp = firebase.firestore.Timestamp.fromDate(new Date());
+    const article: Article = {
+      title,
+      body,
+      genre,
+      created_at: timestamp,
+      updated_at: timestamp
+    };
+    await firestore.collection("articles").add(article);
+  }, [title, body]);
+
+  return (
+    <div className="Add">
+      <input
+        type="text"
+        defaultValue={title}
+        onChange={e => setTitle(e.currentTarget.value)}
+      />
+      <input
+        type="text"
+        defaultValue={genre}
+        onChange={e => setGenre(e.currentTarget.value)}
+      />
+      <textarea
+        defaultValue={body}
+        onChange={e => setBody(e.currentTarget.value)}
+      />
+      <button onClick={post}>POST</button>
+    </div>
+  );
+};
+
+export default Add;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Page from "../components/Page/Page";
 import Index from "./Index/Index";
 import SignIn from './SignIn/SignIn';
+import Add from "./Add/Add";
 import Article from "./Article/Article";
 
 const Pages = () => {
@@ -11,6 +12,7 @@ const Pages = () => {
       <Switch>
         <Route path="/" exact component={Index} />
         <Route path='/signin' component={SignIn} />
+        <Route path="/add" component={Add} />
         <Route path="/:genre/:title" exact component={Article} />
       </Switch>
     </Page>
